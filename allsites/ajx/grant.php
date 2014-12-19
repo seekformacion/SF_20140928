057312cbdb84131893a10bbe544acbd04acb48bb
eduardo <2010dos>
2014-12-20 00:05:13 +0100

    firewall

--- allsites/ajx/grant.php
+++ allsites/ajx/grant.php
@@ -3,13 +3,13 @@
 
 require_once ('iniAJX.php');
 
-if(isset($_GET['ip'])) {
-    $ip=($_GET['ip'];
+if(isset($_GET['ip'])) {if(strlen($_GET['ip'])>6){
+    $ip=$_GET['ip'];
 
     DBUpInsSDB("INSERT INTO rules (tipo,ip) VALUES ('d','$ip');",'frwrules');
 
     echo $ip;
-}
+}}
 
 
 ?>
